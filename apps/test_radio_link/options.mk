17d3a9f367f923d866b8430f84b4b3e229d8e63d
David (Pololu) <dev-david@pololu.com>
2011-02-24 12:47:19 -0800

    Created test_radio_link, an app that will help us fix problems in the radio_link library by giving us useful debugging information.  Currently it seems to be working just fine except for that bug in radio_link that we are hunting down.

---
+++ apps/test_radio_link/options.mk
@@ -0,0 +1,1 @@
+APP_LIBS := usb_cdc_acm.lib usb.lib radio_link.lib radio_mac.lib radio_registers.lib wixel.lib random.lib dma.lib
---
+++ apps/test_radio_link/test_radio_link.c
@@ -0,0 +1,123 @@
+// test2: David is trying to do something more advanced, which requires
+// a little more coordination between Wixels.  The first step will be
+// to allow them to agree on time slots.
+
+#include <cc2511_map.h>
+#include <board.h>
+#include <usb.h>
+#include <usb_com.h>
+#include <radio_link.h>
+#include <random.h>
+#include <stdio.h>
+
+void blinkLeds()
+{
+    usbShowStatusWithGreenLed();
+
+    if (MARCSTATE == 0x11)
+    {
+        LED_RED(1);
+    }
+    else
+    {
+        LED_RED(0);
+    }
+}
+
+uint8 nibbleToAscii(uint8 nibble)
+{
+    nibble &= 0xF;
+    if (nibble <= 0x9){ return '0' + nibble; }
+    else{ return 'A' + (nibble - 0xA); }
+}
+
+void radioToUsb()
+{
+	uint8 XDATA buffer[128];
+	uint8 length;
+	uint8 i;
+
+	uint8 XDATA * packet = radioLinkRxCurrentPacket();
+	if (packet == 0){ return; }
+
+	if (usbComTxAvailable() < packet[0]*2 + 30){ return; }
+
+	length = sprintf(buffer, "RX: (");
+	buffer[length++] = nibbleToAscii(packet[1] >> 4);
+	buffer[length++] = nibbleToAscii(packet[1]);
+	buffer[length++] = ')';
+	for (i = 0; i < packet[0]; i++)
+	{
+		buffer[length++] = nibbleToAscii(packet[RADIO_LINK_PACKET_DATA_OFFSET+i] >> 4);
+		buffer[length++] = nibbleToAscii(packet[RADIO_LINK_PACKET_DATA_OFFSET+i]);
+	}
+
+	buffer[length++] = '\r';
+	buffer[length++] = '\n';
+
+	radioLinkRxDoneWithPacket();
+	usbComTxSend(buffer, length);
+}
+
+void handleCommands()
+{
+	uint8 XDATA txNotAvailable[] = "TX not available!\r\n";
+	uint8 XDATA response[128];
+	uint8 responseLength;
+	if (usbComRxAvailable() && usbComTxAvailable() >= 50)
+	{
+		uint8 byte = usbComRxReceiveByte();
+		if (byte == (uint8)'?')
+		{
+			responseLength = sprintf(response, "? Hello\r\n");
+			usbComTxSend(response, responseLength);
+		}
+		else if (byte >= (uint8)'a' && byte <= (uint8)'g')
+		{
+			uint8 XDATA * packet = radioLinkTxCurrentPacket();
+			if (packet == 0)
+			{
+				usbComTxSend(txNotAvailable, sizeof(txNotAvailable));
+			}
+			else
+			{
+				packet[RADIO_LINK_PACKET_DATA_OFFSET] = byte;
+				radioLinkTxSendPacket(1);
+				responseLength = sprintf(response, "TX: %02x\r\n", byte);
+				usbComTxSend(response, responseLength);
+			}
+		}
+	}
+}
+
+void main()
+{
+    wixelInit();
+    usbInit();
+
+    radioLinkInit();
+    randomSeedFromAdc();
+
+    // Set up P1_6 to be the RX debug signal and P1_7 to be the TX debug signal.
+    P1DIR |= (1<<6) | (1<<7);
+    IOCFG1 = 0b001000; // P1_6 = Preamble Quality Reached
+    IOCFG2 = 0b011011; // P1_7 = PA_PD (TX mode)
+
+    while(1)
+    {
+        wixelService();
+        blinkLeds();
+        radioToUsb();
+        handleCommands();
+        usbComService();
+    }
+}
+
+// Local Variables: **
+// mode: C **
+// c-basic-offset: 4 **
+// tab-width: 4 **
+// indent-tabs-mode: nil **
+// end: **
+
+
