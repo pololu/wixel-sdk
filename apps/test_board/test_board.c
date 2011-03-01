e2f6448a1eded2e2e10f078563e693d23839877d
David (Pololu) <dev-david@pololu.com>
2011-03-01 12:21:56 -0800

    test_board.c: Switched the assignment of the red and yellow LEDs so it is possible to get the board in to bootloader mode using a wire while the app is running.

--- apps/test_board/test_board.c
+++ apps/test_board/test_board.c
@@ -1,12 +1,18 @@
 /* test_board: This is a simple test application that test the basic
  * features of board.h and time.h.
  * 
+ * It blinks the green LED, but that is only visible if the USB
+ * is connected because the green LED is powered from USB.
+ * 
+ * It turns on the yellow LED if and only if self power is present.
+ * 
+ * It turns on the red LED if and only if USB power is present.
+ * 
  * Note that this app does NOT implement a USB interface, so it will
  * not be recognized by the Wixel Configuration Utility and you can
  * not get it in to bootloader mode using a USB command.  However, it
  * does call boardService(), so you can get it in to bootloader mode
  * by shorting P2_2 to 3V3 (3.3 V).
- * 
  */
 
 #include <board.h>
@@ -14,9 +20,9 @@
 
 void updateLeds()
 {
-    LED_GREEN(timeMs >> 9 & 1);     // Blink the Green LED.
-    LED_YELLOW(usbPowerPresent());  // Indicate USB power using the yellow LED.
-    LED_RED(vinPowerPresent());     // Indicate VIN with the red LED.
+    LED_GREEN(timeMs >> 9 & 1);     // Blink the Green LED (only visible w/ USB).
+    LED_YELLOW(vinPowerPresent());  // Indicate VIN power with the yellow LED.
+    LED_RED(usbPowerPresent());     // Indicate USB power with the red LED.
 }
 
 void main()
