5c6a9373d48c486d1e7daf5818d6c7e7d98dcf2f
David (Pololu) <dev-david@pololu.com>
2011-03-15 10:53:44 -0700

    example_blink_led app:  Removed the serial commands to make it simpler.

--- apps/example_blink_led/example_blink_led.c
+++ apps/example_blink_led/example_blink_led.c
@@ -4,17 +4,15 @@
 #include <usb_com.h>
 #include <stdio.h>
 
-int32 CODE param_blink_period_ms = 500;
+int32 CODE param_blink_period_ms = 1000;
 
-BIT yellowLedOn = 0;
+uint32 lastToggle = 0;
 
 void updateLeds()
 {
-    static uint32 lastToggle = 0;
-
     usbShowStatusWithGreenLed();
 
-    LED_YELLOW(yellowLedOn);
+    LED_YELLOW(0);
 
     // NOTE: The code below is bad because it is reading two bytes of timeMs,
     // and the interrupt that updates timeMs could fire between those two reads.
@@ -22,36 +20,6 @@ void updateLeds()
     {
         LED_RED(!LED_RED_STATE);
         lastToggle = getMs();
-    }
-}
-
-void receiveCommands()
-{
-    if (usbComRxAvailable() && usbComTxAvailable() >= 64)
-    {
-        uint8 XDATA response[64];
-        uint8 byte;
-        uint8 length;
-        byte = usbComRxReceiveByte();
-        switch(byte)
-        {
-        // Commands for toggling the yellow LED.
-        case 'y':
-        case 0x81:
-            yellowLedOn = !yellowLedOn;
-            break;
-
-        // Commands for getting the blink_period parameter.
-        case 'b':
-            length = sprintf(response, "blink period = %d ms\r\n", (uint16)param_blink_period_ms);
-            usbComTxSend(response, length);
-            break;
-        case 0x82:
-            response[0] = param_blink_period_ms & 0xFF;
-            response[1] = param_blink_period_ms >> 8 & 0xFF;
-            usbComTxSend(response, 2);
-            break;
-        }
     }
 }
 
@@ -65,6 +33,5 @@ void main()
         boardService();
         updateLeds();
         usbComService();
-        receiveCommands();
     }
 }
