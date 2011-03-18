55ff5d8ff4ab15baac2ca064c2440e742058dc74
David (Pololu) <dev-david@pololu.com>
2011-03-17 18:04:50 -0700

    Added make_all.bat

---
+++ make_all.bat
@@ -0,0 +1,8 @@
+:: This Windows batch file will build all of the apps
+:: and the libraries that they depend on.
+:: You can run it by double-clicking on it.
+
+@echo off
+make
+echo You may now close this window.
+pause > nul
