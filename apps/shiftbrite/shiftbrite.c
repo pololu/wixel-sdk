4c2e7dbdd640c28930acc799d4009f9d0322c03c
Paul (Pololu) <dev-paul@pololu.com>
2011-03-25 19:47:27 -0700

    shiftbrite app: basic code that is supposed to turn on the LED but for some reason causes it to flicker randomly

---
+++ apps/shiftbrite/shiftbrite.c
@@ -0,0 +1,87 @@
+#include <wixel.h>
+#include <usb.h>
+#include <usb_com.h>
+#include <stdio.h>
+
+int32 CODE param_blink_period_ms = 500;
+
+uint32 lastToggle = 0;
+
+void updateLeds()
+{
+    usbShowStatusWithGreenLed();
+
+    LED_YELLOW(0);
+
+    if ((uint16)(getMs() - lastToggle) >= (param_blink_period_ms/2))
+    {
+        LED_RED(!LED_RED_STATE);
+        lastToggle = getMs();
+    }
+}
+
+void toggleLatch()
+{
+    delayMs(1);
+    P1_5 = 1;
+    delayMs(1);
+    P1_5 = 0;
+}
+
+void sendBit(BIT value)
+{
+    P1_6 = value;
+    delayMs(1);
+    P1_5 = 1;
+    delayMs(1);
+    P1_5 = 0;
+    delayMs(1);
+}
+
+void sendRGB(uint16 r, uint16 g, uint16 b)
+{
+    uint16 mask = 512;
+    sendBit(0);
+    sendBit(0);
+    while(mask)
+    {
+        sendBit((mask & r) ? 1 : 0);
+        mask >>= 1;
+    }
+    mask = 512;
+    while(mask)
+    {
+        sendBit((mask & g) ? 1 : 0);
+        mask >>= 1;
+    }
+    mask = 512;
+    while(mask)
+    {
+        sendBit((mask & b) ? 1 : 0);
+        mask >>= 1;
+    }
+
+    toggleLatch();
+}
+
+void main()
+{
+    systemInit();
+    usbInit();
+
+    P1DIR |= (1<<4); // P1_4 = !Enable
+    P1DIR |= (1<<5); // P1_5 = Clock
+    P1DIR |= (1<<6); // P1_6 = Data
+    P1DIR |= (1<<7); // P1_7 = Latch
+    P1_4 = 0; // enable shiftbrites
+    P1_5 = 0; // clock low
+
+    while(1)
+    {
+        sendRGB(1,1,1);
+
+        boardService();
+        updateLeds();
+        usbComService();
+    }
+}
