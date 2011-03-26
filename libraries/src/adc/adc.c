bd57a4695b337f99c35076265d32c10bd09e38af
David (Pololu) <dev-david@pololu.com>
2011-03-26 01:03:57 -0700

    adc library: Added advanced options.  Added adcReadDifferential.  Added tons of documentation.

--- libraries/src/adc/adc.c
+++ libraries/src/adc/adc.c
@@ -4,23 +4,29 @@
 
 uint16 adcRead(uint8 channel)
 {
-    int16 result;
     ADCIF = 0;               // Clear the flag.
-    ADCCON3 = 0b10110000 | channel;
+    ADCCON3 = 0b10110000 ^ channel;
     while(!ADCIF){};         // Wait for the reading to finish.
 
-    // The result of the conversion is now sitting in the ADCH and ADCL
-    // registers.  Despite what the datasheet says, this result can
-    // sometimes be negative, and bits 2 and 3 of ADCL are not always
-    // zero (they seem to be pretty random).
-
-    result = ADC;
-    if (result < 0)
+    if (ADCH & 0x80)
     {
+    	// Despite what the datasheet says, the result was negative.
         return 0;
     }
     else
     {
-        return result >> 4;
+        // Note: Despite what the datasheet says, bits 2 and 3 of ADCL are not
+        // always zero (they seem to be pretty random).  We throw them away
+        // here.
+        return ADC >> 4;
     }
+}
+
+int16 adcReadDifferential(uint8 channel)
+{
+	ADCIF = 0;               // Clear the flag.
+    ADCCON3 = 0b10110000 ^ channel;
+    while(!ADCIF){};         // Wait for the reading to finish.
+
+    return (int16)ADC >> 4;
 }
