beff2d5bf2a4a6ab20b2650b705b1bcebf338b21
David (Pololu) <dev-david@pololu.com>
2011-04-05 10:17:09 -0700

    test_board app: Improved a comment.

--- apps/test_board/test_board.c
+++ apps/test_board/test_board.c
@@ -12,7 +12,7 @@
  * not be recognized by the Wixel Configuration Utility and you can
  * not get it in to bootloader mode using a USB command.  However, it
  * does call boardService(), so you can get it in to bootloader mode
- * by shorting P2_2 to 3V3 (3.3 V).
+ * by shorting P2_2 to 3V3 (3.3 V) as long as the yellow LED is off.
  */
 
 #include <board.h>
