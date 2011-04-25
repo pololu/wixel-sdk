471fe85339f833e3b5a89e9979fbfbea5fcecdc4
Kevin (Pololu) <dev-kevin@pololu.com>
2011-04-25 14:43:58 -0700

    i2c lib: .h file with corresponding changes

--- libraries/include/i2c.h
+++ libraries/include/i2c.h
@@ -5,7 +5,11 @@
 
 extern BIT i2cTimeoutOccurred;
 
-void i2cInit(uint16 freqKHz, uint16 timeoutMs);
+void i2cSetFrequency(uint16 freqKHz);
+void i2cSetTimeout(uint16 timeoutMs);
+
+void i2cStart();
+void i2cStop();
 
 BIT i2cWriteByte(uint8 byte, uint8 sendStart, uint8 sendStop);
 uint8 i2cReadByte(uint8 nack, uint8 sendStop);
