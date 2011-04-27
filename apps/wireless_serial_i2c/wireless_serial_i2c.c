48eb9effd2dcc0ff7d82449dc2ff6478fe1ef7f1
Kevin (Pololu) <dev-kevin@pololu.com>
2011-04-26 18:34:19 -0700

    wireless_serial_i2c.c: minor formatting & comment edits

--- apps/wireless_serial_i2c/wireless_serial_i2c.c
+++ apps/wireless_serial_i2c/wireless_serial_i2c.c
@@ -1,77 +1,13 @@
-/* wireless_serial app:
+/* wireless_serial_i2c app:
  *
  * Pin out:
  * P0_3 = TX
  * P0_2 = RX
- * P1_7 = Radio Transmit Debug Signal
- * P1_6 = Radio Receive Debug Signal
+ * P1_0 = I2C SCL
+ * P1_1 = I2C SDA
  *
- * == Overview ==
- * This app allows you to connect two Wixels together to make a wireless,
- * bidirectional, lossless serial link.  The Wixels must be on the same radio
- * channel, and all other pairs of Wixels must be at least 2 channels away.
- *
- * == Technical Description ==
- * This device appears to the USB host as a Virtual COM Port, with USB product
- * ID 0x2200.  It uses the radio_link library to do wireless communication.
- *
- * There are three basic serial modes that can be selected:
- * 1) USB-to-Radio: Bytes from the USB virtual COM port get sent to the
- *    radio and vice versa.
- * 2) UART-to-Radio: Bytes from the UART's RX line get sent to the radio
- *    and bytes from the radio get sent to the UART's TX line.
- * 3) USB-to-UART: Just like a normal USB-to-Serial adapter, bytes from
- *    the virtual COM port get sent on the UART's TX line and bytes from
- *    the UART's RX line get sent to the virtual COM port.
- *
- * You can select which serial mode you want to use by setting the bridge_mode
- * parameter to the appropriate number (using the numbers above).  Or, you can
- * leave the serial mode at 0 (which is the default).  If the bridge_mode is 0,
- * then the Wixel will automatically choose a serial mode based on how it is
- * being powered, and it will switch between the different serial modes on the
- * fly.
- *
- * Power Source | Serial Mode
- * --------------------------
- * USB only     | USB-to-Radio
- * VIN only     | UART-to-Radio
- * USB and VIN  | USB-to-UART
- *
- * == Parameters ==
- *   bridge_mode   : Selects the serial mode or auto mode (0-3).
- *   baud_rate     : The baud rate to use for the UART, in bits per second.
- *   radio_channel : See description in radio_link.h.
- *
- * == Example Uses ==
- * 1) This application can be used to make a wireless serial link between two
- *    microcontrollers, with no USB involved.  To do this, use the UART-to-Radio
- *    mode on both Wixels.
- *
- * 2) This application can be used to make a wireless serial link between a
- *    computer and a microcontroller.  Use USB-to-Radio mode on the Wixel that
- *    is connected to the computer and use UART-to-Radio mode on the Wixel
- *    that is connected to the microcontroller.
- *
- * 3) If you are doing option 2 and using the the auto-detect serial mode
- *    (bridge_mode = 0), then you have the option to (at any time) plug a USB
- *    cable directly in to the Wixel that is connected to your microcontroller
- *    to establish a more direct (wired) serial connection with the
- *    microcontroller.  (You would, of course, also have to switch to the other
- *    COM port when you do this.)
- */
-
-// TODO: try harder to enable a pull-up on the RX line.  Right now, we get junk
-// on the RX line from 60 Hz noise if you connect one end of a cable to the RX line.
-
-/*
- * TODO: Support for USB CDC ACM control signals.
- * TODO: use LEDs to give feedback about sending/receiving bytes.
- * TODO: UART flow control.
- * TODO: Better radio protocol (see TODOs in radio_link.c).
- * TODO: Obey CDC-ACM Set Line Coding commands:
- *       In USB-UART mode this would let the user change the baud rate at run-time.
- *       In USB-RADIO mode, bauds 0-255 would correspond to radio channels.
- */
+ * This app allows you use a Wixel as an I2C bus master, controlled by a
+ * wireless, UART, or USB interface.
 
 /** Dependencies **************************************************************/
 #include <cc2511_map.h>
@@ -125,10 +61,10 @@ static BIT dataDirIsRead = 0;
 static BIT lengthSet = 0;
 static uint8 dataLength = 0;
 
-uint8 (*rxAvailableFunction)(void) = NULL;
+uint8 (*rxAvailableFunction)(void)   = NULL;
 uint8 (*rxReceiveByteFunction)(void) = NULL;
-uint8 (*txAvailableFunction)(void) = NULL;
-void (*txSendByteFunction)(uint8) = NULL;
+uint8 (*txAvailableFunction)(void)   = NULL;
+void  (*txSendByteFunction)(uint8)   = NULL;
 
 /** Functions *****************************************************************/
 
