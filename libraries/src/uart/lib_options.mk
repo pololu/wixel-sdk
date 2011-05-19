c2ca05e23080992dd1273181c7dae239b4bfc544
David (Pololu) <dev-david@pololu.com>
2011-05-19 11:02:18 -0700

    uart library: Made it so that "make clean" removes the auto-generated files uart0.c and uart1.c.

--- libraries/src/uart/lib_options.mk
+++ libraries/src/uart/lib_options.mk
@@ -1,18 +1,17 @@
-# Compile uart0.rel and uart1.rel from uart.c
+# This library will be made by linking uart0.rel and uart1.rel.
 LIB_RELS := libraries/src/uart/uart0.rel libraries/src/uart/uart1.rel
 
+# When those rel (object) files are compiled, there will be a
+# special preprocessor flag to specify which UART to use.
 libraries/src/uart/uart0.rel : C_FLAGS += -DUART0
 libraries/src/uart/uart1.rel : C_FLAGS += -DUART1
 
-#libraries/src/uart/uart0.rel : libraries/src/uart/uart.c
-#	$(COMPILE_COMMAND)
-
-#libraries/src/uart/uart1.rel : libraries/src/uart/uart.c
-#	$(COMPILE_COMMAND)
-
+# The rel files will be compiled from uart0.c and uart1.c,
+# which will both be copies of core/uart.c.
 libraries/src/uart/uart0.c : libraries/src/uart/core/uart.c
 	$(CP) $< $@
 	
 libraries/src/uart/uart1.c : libraries/src/uart/core/uart.c
 	$(CP) $< $@
 
+TARGETS += libraries/src/uart/uart0.c libraries/src/uart/uart1.c
