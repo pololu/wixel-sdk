a249113101eb76feee7c5b02d570443c996b077f
David (Pololu) <dev-david@pololu.com>
2011-08-19 10:59:32 -0700

    wireless_adc_rx app: Improved documentation and comments.

--- apps/wireless_adc_rx/wireless_adc_rx.c
+++ apps/wireless_adc_rx/wireless_adc_rx.c
@@ -2,16 +2,16 @@
 
 This app wirelessly receives ADC (Analog-to-Digital Converter) readings from
 Wixels running the wireless_adc_tx app, formats them in ASCII, and sends them
-to the virtual COM port so you can see them on a PC.
+to the virtual COM port so they can be displayed or processed on a PC.
 
-You can use this app, along with the wirless_adc_tx transmitter app, to remotely
+You can use this app, along with the wireless_adc_tx transmitter app, to wirelessly
 read the value of any voltage.  For example, the voltage could be from the
 analog output of any sensor, such as an accelerometer, gyro, or distance sensor.
 
 This app supports having multiple transmitters and multiple receivers.
 
 NOTE: The Wixel's ADC inputs can only tolerate voltages between 0 (GND) and
-3.3 V.  If you need to measure voltages over a wider range, you will need
+3.3 V.  To measure voltages over a wider range, you will need
 additional circuitry (such as a voltage divider).
 
 == Getting Started ==
@@ -22,19 +22,23 @@ on each Wixel: P0_0, P0_1, P0_2, P0_3, P0_4, or P0_5.  Make sure that the ground
 (GND) pins of the Wixel and the device producing the analog voltage are connected,
 and ensure that everything is sufficiently powered.
 
-Second, load the wireless_adc_rx app onto a Wixel and connect it to a
+Next, load the wireless_adc_rx app onto a Wixel and connect it to a
 PC via USB.  Open the COM port of that Wixel using a terminal program.
-The terminal programs should print (display) a steady stream of lines that look
-something like this:
+The terminal programs should print (display) a steady stream of lines that have
+the following format:
 
-07-37-8B-69 12697   226   226   1501   226   226   226
+07-37-8B-69 12697   226   226   1501  2931   226   226
+ (Serial)   (Time)  (0)   (1)    (2)   (3)   (4)   (5)
 
-The first number (07-37-8B-69) is the serial number of the Wixel that
-measured the votlages.
-The second number is a 16-bit measure of the time when the receiver
-processed the packet, in milliseconds.
-The third through seventh numbers are the respective voltages on the
-P0_0, P0_1, P0_2, P0_3, P0_4, and P0_5, in units of millivolts.
+(Serial) The serial number of the Wixel that measured the voltages.
+(Time)   A 16-bit measure of the time when the receiver processed the report, in
+         milliseconds (ms).
+(0-5)    The voltages measured on P0_0, P0_1, P0_2, P0_3, P0_4, and P0_5 in
+         units of millivolts (mV).
+
+To use this data in your application, you can either write PC software that
+reads the data from the COM port and does something with it, or you can
+modify this app.
 
 */
 
@@ -77,7 +81,7 @@ void radioToUsbService()
     if ((rxPacket = (adcReport XDATA *)radioQueueRxCurrentPacket()) && usbComTxAvailable() >= 64)
     {
         // We received a packet from a Wixel, presumably one running
-        // the wireless_adc_rx app.  Format it nicely and send it to
+        // the wireless_adc_tx app.  Format it nicely and send it to
         // the USB host (PC).
 
         uint8 i;
