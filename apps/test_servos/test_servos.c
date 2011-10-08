660caf4bd7f6db409ace836f38c8355b203ccbff
David (Pololu) <dev-david@pololu.com>
2011-10-07 18:20:21 -0700

    test_servo app: The jittering was due to using the internal oscillator.  Switched to 24 MHz and now there is less than 1 ns of jitter.

--- apps/test_servos/test_servos.c
+++ apps/test_servos/test_servos.c
@@ -2,16 +2,22 @@
 
 void main()
 {
-    PERCFG |= (1<<6);  // PERCFG.T1CFG = 1:  Move Timer 1 to Alt. 2 location (P1_0, P1_1, P1_2)
+    // Set up 24 MHz clock.
+    SLEEP &= ~0x04;
+    while(!(SLEEP & 0x40));
+    CLKCON = 0x80;
+    SLEEP |= 0x04;
 
-    P1SEL |= (1<<1); // tmphax hardware pwm
-    P1DIR |= (1<<1); // tmphax hardware pwm
+    // Set up hardware PWM.
+    PERCFG |= (1<<6);  // PERCFG.T1CFG = 1:  Move Timer 1 to Alt. 2 location (P1_0, P1_1, P1_2)
+    P1SEL |= (1<<1);
+    P1DIR |= (1<<1);
 
     T1CC0 = 60000;         // Period = 60000/24 = 2500 microseconds.
     T1CCTL1 = 0b01010100;  // Timer 1 Channel 1: Compare mode with interrupt enabled.  Toggle output on match.
     T1CTL = 0b00000010;    // Timer 1: Module mode, repeatedly counts from 0 to T1CC0.
 
-    T1CC1 = 500;
+    T1CC1 = 2400;
 
     while(1)
     {
