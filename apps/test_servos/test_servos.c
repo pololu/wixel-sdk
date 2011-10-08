b7adfae27d48016f62ecce58673f2f9ce9aa2a35
David (Pololu) <dev-david@pololu.com>
2011-10-07 18:06:54 -0700

    test_servo app: Simplified it to something very simple that produces jittery hardware PWM signal!

--- apps/test_servos/test_servos.c
+++ apps/test_servos/test_servos.c
@@ -1,60 +1,19 @@
-#include <wixel.h>
-#include <usb.h>
-#include <usb_com.h>
-#include <servo.h>
-
-static uint8 CODE servoPins[] = {14};
-
-void updateLeds()
-{
-    usbShowStatusWithGreenLed();
-    LED_YELLOW(0);
-    T1CNTL;
-    LED_RED(T1CNTH & 0x80 ? 0 : 1);
-}
-
-void updateServos()
-{
-
-}
+#include <cc2511_map.h>
 
 void main()
 {
-    // OSC_PD=0: Power up both high-speed oscillators: XOSC and HS RCOSC.
-    //SLEEP &= ~0x04;
+    PERCFG |= (1<<6);  // PERCFG.T1CFG = 1:  Move Timer 1 to Alt. 2 location (P1_0, P1_1, P1_2)
 
-    // Wait until the high speed crystal oscillator is stable (SLEEP.XOSC_STB=1)
-    // This condition is required to use the radio or USB.
-    // According to Table 6.4.2, the waiting should take about 650 microseconds.
-    //while(!(SLEEP & 0x40));
+    P1SEL |= (1<<1); // tmphax hardware pwm
+    P1DIR |= (1<<1); // tmphax hardware pwm
 
-    // OSC32K = 1: Use low power 32kHz oscillator for the 32 kHz signal.
-    // OSC=0: Select high speed crystal oscillator for system clock (24 MHz).
-    // TICKSPD=000: Set the timer tick speed to its fastest possible value.
-    // CLKSPD=000: Set system clock speed to its fastest possible value (24 MHz).
-    //    This is required for using the Forward Error Correction radio feature (which we don't use anymore).
-    //CLKCON = 0x80;
+    T1CC0 = 60000;         // Period = 60000/24 = 2500 microseconds.
+    T1CCTL1 = 0b01010100;  // Timer 1 Channel 1: Compare mode with interrupt enabled.  Toggle output on match.
+    T1CTL = 0b00000010;    // Timer 1: Module mode, repeatedly counts from 0 to T1CC0.
 
-    // Power down the HS RCOSC (the one that is not currently selected by
-    // CLKCON.OSC).
-    //SLEEP |= 0x04;
-
-    //boardIoInit();
-    //boardClockInit();
-    //timeInit();
-    //dmaInit();
-
-    //usbInit();
-    servosStart((uint8 XDATA *)servoPins, sizeof(servoPins));
+    T1CC1 = 500;
 
     while(1)
     {
-        __asm nop nop nop nop nop nop nop nop nop nop nop nop __endasm;
-        __asm nop nop nop nop nop nop nop nop nop nop nop nop __endasm;
-        __asm nop nop nop nop nop nop nop nop nop nop nop nop __endasm;
-        //boardService();
-        //usbComService();
-        //updateLeds();
-        //updateServos();
     }
 }
