7c99b9133d41d59db2803b7daab38b2af3e54cca
David (Pololu) <dev-david@pololu.com>
2011-10-10 13:16:54 -0700

    test_servos app: Decided to do hardware PWM instead and started working on that.

--- apps/test_servos/test_servos.c
+++ apps/test_servos/test_servos.c
@@ -1,54 +1,71 @@
 #include <cc2511_map.h>
 #include <servo.h>
 #include <wixel.h>
+#include <usb.h>
+#include <usb_com.h>
 
-void mainLoop();
+volatile uint8 DATA servoCounter = 0;
+
+ISR(T1,0)
+{
+    switch(servoCounter++)
+    {
+    case 0:
+        //PERCFG |= (1<<6);  // PERCFG.T1CFG = 1:  Move Timer 1 to Alt. 2 location (P1_2, P1_1, P1_0)
+        //T1CC0 = 1;
+        //T1CC1 = -25000;
+        //T1CC2 = -26000;
+        break;
+    case 1:
+        T1CC0 = T1CC1 = T1CC2 = 0xFFFF;
+        break;
+
+    case 2:
+        //PERCFG &= ~(1<<6);  // PERCFG.T1CFG = 0:  Move Timer 1 to Alt. 1 location (P0_2, P0_3, P0_4)
+        T1CC0 = -30000;
+        T1CC1 = -32000;
+        T1CC2 = 0x10000-34000;
+        break;
+
+    case 3:
+        T1CC0 = T1CC1 = T1CC2 = 0xFFFF;
+        break;
+
+    case 7:
+        servoCounter = 0;
+        break;
+    }
+
+}
 
 void main()
 {
-    // Set up 24 MHz clock.
-    //boardIoInit();
+    systemInit();
+    usbInit();
 
-    P2 = 0b00000110;     // P2_1 = 1: drive the red LED line high LATER (when LED_RED(1) is called)
-                         // P2_2 = 1: drive the yellow LED line high LATER (when LED_YELLOW(1) is called)
-                         // P2_4 = 0: drive the VBUS_IN/GREEN_LED line low LATER (when LED_GREEN(1) is called)
-    P2INP = 0b10011001;  // Pull down LED pins (P2_2, P2_1), and tristate the other Port 2 pins.
-
-    // OSC_PD=0: Power up both high-speed oscillators: XOSC and HS RCOSC.
-    SLEEP &= ~0x04;
-
-    // Wait until the high speed crystal oscillator is stable (SLEEP.XOSC_STB=1)
-    // This condition is required to use the radio or USB.
-    // According to Table 6.4.2, the waiting should take about 650 microseconds.
-    while(!(SLEEP & 0x40));
-
-    // OSC32K = 1: Use low power 32kHz oscillator for the 32 kHz signal.
-    // OSC=0: Select high speed crystal oscillator for system clock (24 MHz).
-    // TICKSPD=000: Set the timer tick speed to its fastest possible value.
-    // CLKSPD=000: Set system clock speed to its fastest possible value (24 MHz).
-    //    This is required for using the Forward Error Correction radio feature (which we don't use anymore).
-    CLKCON = 0x80;
-
-    // Power down the HS RCOSC (the one that is not currently selected by
-    // CLKCON.OSC).
-    SLEEP |= 0x04;
-
-    MEMCTR = 2;
-
-
-    //boardClockInit();
-    //dmaInit();
+    P2SEL |= (1<<3); // might not be necessary, makes Timer 1 have priority over USART1
 
     // Set up hardware PWM.
-    PERCFG |= (1<<6);  // PERCFG.T1CFG = 1:  Move Timer 1 to Alt. 2 location (P1_0, P1_1, P1_2)
-    P1SEL |= (1<<1);
-    P1DIR |= (1<<1);
+    PERCFG &= ~(1<<6);  // PERCFG.T1CFG = 0:  Move Timer 1 to Alt. 1 location (P0_2, P0_3, P0_4)
+    P1SEL |= 0b111;
+    P1DIR |= 0b111;
+
+    P0SEL |= 0b11100;
+    P0DIR |= 0b11100;
 
     T1CC0 = 60000;         // Period = 60000/24 = 2500 microseconds.
-    T1CCTL1 = 0b01011100;  // Timer 1 Channel 1: Compare mode with interrupt enabled.  Toggle output on match.
-    T1CTL = 0b00000010;    // Timer 1: Module mode, repeatedly counts from 0 to T1CC0.
 
-    T1CC1 = 2400;
+    // Configure Timer 1 Channels 0-2 to be in compare mode.  Set output on compare-up, clear on 0.
+    // With this configuration, we can set T1CC0, T1CC1, or T1CC2 to -N to get a pulse of with N,
+    // as long as N > 1.
+    // We can set the register to -1 or 0 to disable the pulse.
+    T1CCTL0 = T1CCTL1 = T1CCTL2 = 0b00011100;
+
+    T1CTL = 0b00000001;    // Timer 1: Start free-running mode, counting from 0x0000 to 0xFFFF.
+
+    T1CC0 = -1;
+    T1CC1 = -9;
+    T1CC2 = -15;
 
     IP0 |= (1<<1);
     IP1 |= (1<<1);
@@ -60,9 +77,6 @@ void main()
     while(1)
     {
         boardService();
+        usbComService();
     }
-
-    //__asm nop __endasm;
-
-    //mainLoop();
 }
--- libraries/src/servo/servo_isr.s
+++ libraries/src/servo/servo_isr.s
@@ -3,24 +3,8 @@
     .area CSEG (CODE)
 
     _P1_4 = 0x94
-
-    .globl _mainLoop
-_mainLoop:
-    ljmp _mainLoop
     
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-
     ; Servo ISR
     .globl _ISR_T1
 _ISR_T1:
-    cpl _P1_4
     reti
