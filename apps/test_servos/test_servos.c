5f9616dc438d853e9d568a8cacaf52ad989b370e
David (Pololu) <dev-david@pololu.com>
2011-10-13 16:14:59 -0700

    test_servos app: Changed the pulse widths and speeds again.

--- apps/test_servos/test_servos.c
+++ apps/test_servos/test_servos.c
@@ -10,36 +10,36 @@ void myServosInit()
 {
     servosStart((uint8 XDATA *)pins, sizeof(pins));
 
-    servoSetSpeed(0, 400);
-    servoSetSpeed(1, 400);
-    servoSetSpeed(2, 400);
-    servoSetSpeed(3, 400);
+    servoSetSpeed(0, 300);
+    servoSetSpeed(1, 300);
+    servoSetSpeed(2, 300);
+    servoSetSpeed(3, 300);
     servoSetSpeed(4, 0);
 
     servoSetSpeed(5, 0);
-    servoSetTarget(5, 1100);
+    servoSetTarget(5, 1000);
     servoSetSpeed(5, 1);
-    servoSetTarget(5, 1900);
+    servoSetTarget(5, 2000);
 }
 
 void updateServos()
 {
     if (getMs() >> 11 & 1)
     {
-        servoSetTarget(0, 1100);
+        servoSetTarget(0, 1000);
         servoSetTarget(1, 1500);
         servoSetTarget(2, 1500);
         servoSetTarget(3, 0);
-        servoSetTarget(4, 1100);
+        servoSetTarget(4, 1900);
         LED_YELLOW(0);
     }
     else
     {
-        servoSetTarget(0, 1900);
-        servoSetTarget(1, 1900);
-        servoSetTarget(2, 1100);
-        servoSetTarget(3, 1600);
-        servoSetTarget(4, 1900);
+        servoSetTarget(0, 2000);
+        servoSetTarget(1, 2000);
+        servoSetTarget(2, 1000);
+        servoSetTarget(3, 1700);
+        servoSetTarget(4, 1100);
         LED_YELLOW(1);
     }
 }
