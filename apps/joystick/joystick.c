1a8c494dae25387c038f21b80f0e09eaa3f3c98f
Kevin (Pololu) <dev-kevin@pololu.com>
2013-08-07 12:05:34 -0700

    joystick app: cleaned up comments; renamed some params; removed axis enable params

--- apps/joystick/joystick.c
+++ apps/joystick/joystick.c
@@ -1,49 +1,30 @@
-/** joystick.c:
-
- This configurable app allows one to build a custom HID joystick
- using the Wixel. The joystick appears as an HID device over a (wired) USB connection.
-
- Configurable features include:
-
-    - Pull types (high or low) for ports 0 and 1
-    - Pin enable options for P0_0 through P0_5 and P1_0 through P1_7
-    - Assignable buttons to pins
-    - Button inversion options
-    - Axis inversion for {x,y,z,Rx,ry,rz}
-    - Axis enable options for {x,y,z,rx,ry,rz}
-    - Assignable axis {x,y,z,rx,ry,rz} to pins P0_0 through P0_5
-
- The joystick axes {x,y,z,rx,ry,rz} use analog pins P0_0 through P0_5.
- If the joystick does not need all these axes, then the analog pins
- can be used as digital buttons instead. The user can also reassign which axis
- uses which analog pin on the Wixel.
-
-*/
+/* joystick app:
+ *
+ * This app allows you to build a custom USB joystick using the Wixel.
+ * The joystick appears as a USB HID (Human Interface Device) class
+ * device over a (wired) USB connection.
+ *
+ * Configuration options include:
+ *
+ * - Mapping of joystick buttons to digital I/O pins
+ * - Mapping of joystick axes to analog inputs
+ * - Button and axis inversion options
+ * - Pull-up/pull-down enable for individual buttons; pull direction 
+ *    selectable for all buttons on each port
+ *
+ * Six analog joystick axes (x, y, z, rx, ry, rz) are mapped to analog
+ * pins P0_0 through P0_5 by default. If you don't need all these
+ * axes, then the analog pins can be used as digital inputs for
+ * additional buttons instead.
+ *
+ */
 
 #include <wixel.h>
 #include <usb.h>
 #include <usb_hid.h>
 
-// Each pin can have a pull status of the following:
-// HIGH_IMPEDANCE (0): Disables the internal pull-up and pull-down resistors on that pin.
-// PULLED (1): Enables an internal 20 kOhm pull resistor for that pin.
-int32 CODE param_P0_0_pull_enable = 0;
-int32 CODE param_P0_1_pull_enable = 0;
-int32 CODE param_P0_2_pull_enable = 0;
-int32 CODE param_P0_3_pull_enable = 0;
-int32 CODE param_P0_4_pull_enable = 0;
-int32 CODE param_P0_5_pull_enable = 0;
-
-int32 CODE param_P1_2_pull_enable = 1;
-int32 CODE param_P1_3_pull_enable = 1;
-int32 CODE param_P1_4_pull_enable = 1;
-int32 CODE param_P1_5_pull_enable = 1;
-int32 CODE param_P1_6_pull_enable = 1;
-int32 CODE param_P1_7_pull_enable = 1;
-
-// Assign which buttons belong to which pin.
-// Buttons can only be assigned to pins
-// P1_0 through P1_7.
+// Button to pin assignment (-1 to disable).
+// See gpio.h documentation for details on pin numbers.
 int32 CODE param_button1_pin = 12;
 int32 CODE param_button2_pin = 13;
 int32 CODE param_button3_pin = 14;
@@ -61,7 +42,9 @@ int32 CODE param_button14_pin = -1;
 int32 CODE param_button15_pin = -1;
 int32 CODE param_button16_pin = -1;
 
-// Invert individual buttons.
+// Invert individual button inputs.
+// 0: Not inverted (logic low = released; logic high = pressed)
+// 1: Inverted (logic high = released; logic low = pressed)
 int32 CODE param_button1_invert = 1;
 int32 CODE param_button2_invert = 1;
 int32 CODE param_button3_invert = 1;
@@ -79,34 +62,52 @@ int32 CODE param_button14_invert = 1;
 int32 CODE param_button15_invert = 1;
 int32 CODE param_button16_invert = 1;
 
-// Assign axis to pin.
-int32 CODE param_X_axis_enable = 1;  // X-Axis
-int32 CODE param_Y_axis_enable = 1;  // Y-Axis
-int32 CODE param_Z_axis_enable = 1;  // Z-Axis
-int32 CODE param_RX_axis_enable = 1;  // RX-Axis
-int32 CODE param_RY_axis_enable = 1;  // RY-Axis
-int32 CODE param_RZ_axis_enable = 1;  // RZ-Axis
+// Axis to ADC channel assignment (-1 to disable).
+// Normally, you'll probably want to use channels 0-5 for enabled
+//  axes (for the voltages on P0_0 through P0_5), but see adc.h
+//  documentation for other options.
+int32 CODE param_X_channel = 0;
+int32 CODE param_Y_channel = 1;
+int32 CODE param_Z_channel = 2;
+int32 CODE param_RX_channel = 3;
+int32 CODE param_RY_channel = 4;
+int32 CODE param_RZ_channel = 5;
 
-// Invert axis enable options.
-int32 CODE param_X_axis_invert = 0;  // X-Axis
-int32 CODE param_Y_axis_invert = 0;  // Y-Axis
-int32 CODE param_Z_axis_invert = 0;  // Z-Axis
-int32 CODE param_RX_axis_invert = 0;  // RX-Axis
-int32 CODE param_RY_axis_invert = 0;  // RY-Axis
-int32 CODE param_RZ_axis_invert = 0;  // RZ-Axis
+// Invert analog axes.
+// 0: Not inverted (axis position is proportional to voltage)
+// 1: Inverted (axis position is inversely proportional to voltage) 
+int32 CODE param_X_invert = 0;
+int32 CODE param_Y_invert = 0;
+int32 CODE param_Z_invert = 0;
+int32 CODE param_RX_invert = 0;
+int32 CODE param_RY_invert = 0;
+int32 CODE param_RZ_invert = 0;
 
-// Axis to pin assignment. Only pins P0_0 through P0_5
-// can be used.
-int32 CODE param_X_pin = 0;
-int32 CODE param_Y_pin = 1;
-int32 CODE param_Z_pin = 2;
-int32 CODE param_RX_pin = 3;
-int32 CODE param_RY_pin = 4;
-int32 CODE param_RZ_pin = 5;
+// Pin pull status.
+// 0: High impedance (disables the internal pull-up and pull-down resistors on that pin)
+// 1: Pulled (enables an internal 20 kOhm pull resistor for that pin)
+// Note that P1_0 and P1_1 don't have pull-ups/pull-downs.
+// Port 0 pull-ups/pull-downs are disabled by default to prevent them
+//  from interfering from analog readings.
+int32 CODE param_P0_0_pull_enable = 0;
+int32 CODE param_P0_1_pull_enable = 0;
+int32 CODE param_P0_2_pull_enable = 0;
+int32 CODE param_P0_3_pull_enable = 0;
+int32 CODE param_P0_4_pull_enable = 0;
+int32 CODE param_P0_5_pull_enable = 0;
 
-// Port pull type specifies if the port is pulled up or down.
-int32 CODE param_port0_pull_type = HIGH;
-int32 CODE param_port1_pull_type = HIGH;
+int32 CODE param_P1_2_pull_enable = 1;
+int32 CODE param_P1_3_pull_enable = 1;
+int32 CODE param_P1_4_pull_enable = 1;
+int32 CODE param_P1_5_pull_enable = 1;
+int32 CODE param_P1_6_pull_enable = 1;
+int32 CODE param_P1_7_pull_enable = 1;
+
+// Port pull type; set for all pins on each port.
+// 0: Low (pull-down enabled)
+// 1: High (pull-up enabled)
+int32 CODE param_port0_pull_type = 1;
+int32 CODE param_port1_pull_type = 1;
 
 void updateLeds()
 {
@@ -114,13 +115,11 @@ void updateLeds()
 }
 
 // Updates the axis position.
-void axisUpdate(int32 analogChannel, int32 axisInvert, int32 axisEnable, int16 * axis)
+void axisUpdate(int32 analogChannel, int32 axisInvert, int16 * axis)
 {
     int16 analogValue;
     int8 lastValue;
 
-    if(!axisEnable)
-        return;
     if(0 > analogChannel || analogChannel > 5)
         return;
 
@@ -138,17 +137,17 @@ void joystickService()
     usbHidJoystickInput.buttons = 0;
 
     // Update x-axis position.
-    axisUpdate(param_X_pin, param_X_axis_invert, param_X_axis_enable, &usbHidJoystickInput.x);
+    axisUpdate(param_X_channel, param_X_invert, &usbHidJoystickInput.x);
     // Update y-axis position.
-    axisUpdate(param_Y_pin, param_Y_axis_invert, param_Y_axis_enable, &usbHidJoystickInput.y);
+    axisUpdate(param_Y_channel, param_Y_invert, &usbHidJoystickInput.y);
     // Update z-axis position.
-    axisUpdate(param_Z_pin, param_Z_axis_invert, param_Z_axis_enable, &usbHidJoystickInput.z);
+    axisUpdate(param_Z_channel, param_Z_invert, &usbHidJoystickInput.z);
     // Update rx-axis position.
-    axisUpdate(param_RX_pin, param_RX_axis_invert, param_RX_axis_enable, &usbHidJoystickInput.rx);
+    axisUpdate(param_RX_channel, param_RX_invert, &usbHidJoystickInput.rx);
     // Update ry-axis position.
-    axisUpdate(param_RY_pin, param_RY_axis_invert, param_RY_axis_enable, &usbHidJoystickInput.ry);
+    axisUpdate(param_RY_channel, param_RY_invert, &usbHidJoystickInput.ry);
     // Update rz-axis position.
-    axisUpdate(param_RZ_pin, param_RZ_axis_invert, param_RZ_axis_enable, &usbHidJoystickInput.rz);
+    axisUpdate(param_RZ_channel, param_RZ_invert, &usbHidJoystickInput.rz);
 
     // Set button status.
     usbHidJoystickInput.buttons |= (param_button1_pin != -1 & (isPinHigh(param_button1_pin) ^ param_button1_invert)) << 0;
