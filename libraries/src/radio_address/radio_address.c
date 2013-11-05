4b0d615dc445b164040032dc3e62442fb0cddbb5
B3rn475 <b3rn4@hotmail.it>
2013-11-05 15:44:13 -0800

    Added missing parameter and Comments

--- libraries/src/radio_address/radio_address.c
+++ libraries/src/radio_address/radio_address.c
@@ -1,16 +1,29 @@
+/* radio_address.c:
+ * this is just an utility library that allow to activate the Address Check Hardware
+ * functionality of the processor
+ */
 #include <radio_address.h>
 #include <cc2511_map.h>
 
+/* PARAMETERS *****************************************************************/
+
+int32 CODE param_address = 128;
+
+/* GENERAL FUNCTIONS **********************************************************/
+
+// Initialize the address from the parameter
 void radioAddressInit()
 {
     ADDR = param_address;
 }
 
+// Returns the current Adddress Check configuration
 uint8 addressCheckHWConfiguration()
 {
 	return PKTCTRL1 & 0x03;
 }
 
+// Sets the Adddress Check configuration
 void setAddressCheckHWConfiguration(uint8 configuration)
 {
 	PKTCTRL1 = (PKTCTRL1 & 0xFD) | (configuration & 0x03);
